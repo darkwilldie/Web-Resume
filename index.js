d02e4bd44875bee25f0b5c3f899b8697453aef1f
darkwilldie <darkwilldie99@gmail.com>
2025-07-14 11:58:44 +0800

    Added dark mode of background and contents.

---
+++ index.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById("toggle-dark");
+var hasDarks = document.querySelectorAll(".has-dark, section");
+btn.onclick = function () {
+  const body = document.body;
+  if (body.getAttribute("data-bs-theme") === "dark") {
+    body.setAttribute("data-bs-theme", "light");
+    btn.textContent = "☀";
+  } else {
+    body.setAttribute("data-bs-theme", "dark");
+    btn.textContent = "☾";
+  }
+  for (var i = 0; i < hasDarks.length; i++) {
+    hasDarks[i].classList.toggle("dark");
+  }
+};
