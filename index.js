f069fe55cf59c66b247b21af317c6cc0ead9e34a
darkwilldie <darkwilldie99@gmail.com>
2025-07-15 22:17:25 +0800

    Added navbar with language switch button.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const $navbar = $(".navbar");
 const $btn = $("#toggle-dark");
 const $hasDarks = $(".has-dark, section");
 
@@ -5,9 +6,15 @@ $btn.on("click", function () {
   $hasDarks.toggleClass("dark");
   const $body = $("body");
   if ($body.attr("data-bs-theme") === "dark") {
+    $navbar
+      .removeClass("navbar-dark bg-dark")
+      .addClass("navbar-light bg-light");
     $body.attr("data-bs-theme", "light");
     $btn.text("☀");
   } else {
+    $navbar
+      .removeClass("navbar-light bg-light")
+      .addClass("navbar-dark bg-dark");
     $body.attr("data-bs-theme", "dark");
     $btn.text("⏾");
   }
