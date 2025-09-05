e1395a65feab55fa8dd7340ea6d58e1fdd69a483
darkwilldie <darkwilldie99@gmail.com>
2025-09-05 18:54:45 +0800

    Simplified code logic of dark mode switching

--- index.js
+++ index.js
@@ -3,21 +3,12 @@ const $btn = $("#toggle-dark");
 const $hasDarks = $(".has-dark, section");
 
 $btn.on("click", function () {
-  $hasDarks.toggleClass("dark");
   const $body = $("body");
-  if ($body.attr("data-bs-theme") === "dark") {
-    $navbar
-      .removeClass("navbar-dark bg-dark")
-      .addClass("navbar-light bg-light");
-    $body.attr("data-bs-theme", "light");
-    $btn.text("☀");
-  } else {
-    $navbar
-      .removeClass("navbar-light bg-light")
-      .addClass("navbar-dark bg-dark");
-    $body.attr("data-bs-theme", "dark");
-    $btn.text("⏾");
-  }
+  const isDark = $body.attr("data-bs-theme") === "dark";
+  $hasDarks.toggleClass("dark");
+  $navbar.toggleClass("navbar-dark bg-dark navbar-light bg-light");
+  $btn.text(isDark ? "⏾" : "☀");
+  $body.attr("data-bs-theme", isDark ? "light" : "dark");
 });
 
 let translations = {};
